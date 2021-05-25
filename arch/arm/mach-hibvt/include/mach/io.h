833a29db10916aa047895d14044b5259e9635172
Aleksey Babakhin <tamerlan311@gmail.com>
2021-05-25 12:28:03 +0300

    arch: mach-hibvt: add Hisilicon Hi3518ev20x Hi3521A Hi3531A family
    ARM926T(Single)
    A7(Single)
    A9

--- arch/arm/mach-hibvt/include/mach/io.h
+++ arch/arm/mach-hibvt/include/mach/io.h
@@ -5,8 +5,20 @@
 #include <mach/hi3516a_io.h>
 #endif
 
+#ifdef CONFIG_ARCH_HI3518EV20X
+#include <mach/hi3518ev20x_io.h>
+#endif
+
 #ifdef CONFIG_ARCH_HI3536DV100
 #include <mach/hi3536dv100_io.h>
+#endif
+
+#ifdef CONFIG_ARCH_HI3521A
+#include <mach/hi3521a_io.h>
+#endif
+
+#ifdef CONFIG_ARCH_HI3531A
+#include <mach/hi3531a_io.h>
 #endif
 
 #endif
